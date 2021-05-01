bd7fe5e9080acf37cd4fbe96f48c276ce6780d50
KWON-IRIS <joohee.kwon0@gmail.com>
2021-05-01 13:44:06 +0900

    py 연습

--- week03/practice.py
+++ week03/practice.py
@@ -5,16 +5,16 @@ print('test')
 
 # let const 없음 / 파이썬은 변수 타입이 없음. >> 원래 안뎀
 number = 1
-float_number = 1.1 # js 에서는 일반적으로 floatNumber (개발자 언어 관련임. 케바케)
+float_number = 1.1  # js 에서는 일반적으로 floatNumber (개발자 언어 관련임. 케바케)
 string = '문자열'
-boolean = True # js 에서는 true
+boolean = True  # js 에서는 true
 
 # list, dict 거의 똑같음
 number_list = [1, 2, 3, 4]
-print(number_list[0]) # 1
+print(number_list[0])  # 1
 
-string_dict ={'name': 'kim', 'age': 30 }
-print(string_dict['name']) # kim
+string_dict = {'name': 'kim', 'age': 30}
+print(string_dict['name'])  # kim
 
 # 들여쓰기가 아주 중요함
 if number > 10:
@@ -23,4 +23,27 @@ else:
     print('10보다 작음')
 
 print('상관 x')
-}
+
+for num in number_list:
+    print(num)
+
+
+# 함수
+def func(num):
+    print(num)
+
+
+def sum(num1, num2):
+    result = num1 + num2
+    return result
+
+
+print(sum(10, 20))
+
+# 과일 갯수 세기.
+fruits = ['사과', '배', '배', '감', '수박', '귤', '딸기', '사과', '배', '수박']
+count = 0
+for fruit in fruits:
+    if fruit == '사과':
+        count = count + 1
+print(count)
