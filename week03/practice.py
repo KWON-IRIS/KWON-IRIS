adc28a4a15ba3db59ce6e3c29baa20d85fcfb75d
KWON-IRIS <joohee.kwon0@gmail.com>
2021-05-01 13:35:45 +0900

    py 연습

---
+++ week03/practice.py
@@ -0,0 +1,26 @@
+# 주석
+# js: console.log('test;)
+
+print('test')
+
+# let const 없음 / 파이썬은 변수 타입이 없음. >> 원래 안뎀
+number = 1
+float_number = 1.1 # js 에서는 일반적으로 floatNumber (개발자 언어 관련임. 케바케)
+string = '문자열'
+boolean = True # js 에서는 true
+
+# list, dict 거의 똑같음
+number_list = [1, 2, 3, 4]
+print(number_list[0]) # 1
+
+string_dict ={'name': 'kim', 'age': 30 }
+print(string_dict['name']) # kim
+
+# 들여쓰기가 아주 중요함
+if number > 10:
+    print('10보다 큼')
+else:
+    print('10보다 작음')
+
+print('상관 x')
+}
